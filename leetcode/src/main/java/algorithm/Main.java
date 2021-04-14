8ab3ce964516d3a9b7004f46723e86fdbccccaaa
tonko2 <10cherry07@gmail.com>
2021-04-15 00:12:00 +0900

    Added problem 1108

---
+++ leetcode/src/main/java/algorithm/Main.java
@@ -0,0 +1,8 @@
+package algorithm;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "1.1.1.1";
+        System.out.println(Problem1108.defangIPaddr(str));
+    }
+}
