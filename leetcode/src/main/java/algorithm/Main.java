7b23d52e27e69d8f715d5a5aa573145bd1a80051
tonko2 <10cherry07@gmail.com>
2021-04-14 23:46:08 +0900

    Added gitignore

--- leetcode/src/main/java/algorithm/Main.java
+++ leetcode/src/main/java/algorithm/Main.java
@@ -2,7 +2,7 @@ package algorithm;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4};
+        int[] nums = {1, 2, 3, 4, 5};
         int[] ans = Problem1480.runningSum(nums);
         for(int i=0; i<ans.length; i++) {
             System.out.println(ans[i]);
