108f43abf34803b1837663862dcb8178076f0a4b
tonko2 <10cherry07@gmail.com>
2021-05-02 20:01:58 +0900

    More efficient solution

--- leetcode/src/main/java/algorithm/Problem1365.java
+++ leetcode/src/main/java/algorithm/Problem1365.java
@@ -2,18 +2,22 @@ package algorithm;
 
 public class Problem1365 {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int res[] = new int[nums.length];
+        int[] temp = new int[101];
         for(int i=0; i<nums.length; i++) {
-            int ans = 0;
-            for(int j=0; j<nums.length; j++) {
-                if (i != j) {
-                    if (nums[i] > nums[j]) {
-                        ans++;
-                    }
-                }
-            }
-            res[i] = ans;
+            temp[nums[i]]++;
         }
-        return res;
+        for(int i=1; i<101; i++) {
+            temp[i] += temp[i-1];
+        }
+
+        int[] ans = new int[nums.length];
+        for(int i=0; i<nums.length; i++) {
+            if (nums[i] == 0) {
+                ans[i] = 0;
+            } else {
+                ans[i] = temp[nums[i]-1];
+            }
+        }
+        return ans;
     }
 }
