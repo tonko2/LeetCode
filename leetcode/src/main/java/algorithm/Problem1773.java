21f5c832320e65fa8d24383c4646a99d0132a467
tonko2 <10cherry07@gmail.com>
2021-05-03 13:47:53 +0900

    Add problem 1773

--- leetcode/src/main/java/algorithm/Problem1773.java
+++ leetcode/src/main/java/algorithm/Problem1773.java
@@ -1,4 +1,25 @@
 package algorithm;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Problem1773 {
+
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int ans = 0;
+        Map<String, Integer> map = new HashMap<>();
+        map.put("type", 0);
+        map.put("color", 1);
+        map.put("name", 2);
+        int column = map.get(ruleKey);
+
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).get(column).equals(ruleValue)) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
 }
