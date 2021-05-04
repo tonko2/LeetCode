f06ae0bd17a95e86ca583b2db508a802a6f9dbf0
tonko2 <10cherry07@gmail.com>
2021-05-04 13:05:30 +0900

    One line solution for problem 1662

--- leetcode/src/main/java/algorithm/Problem1662.java
+++ leetcode/src/main/java/algorithm/Problem1662.java
@@ -2,16 +2,6 @@ package algorithm;
 
 public class Problem1662 {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder words1 = new StringBuilder();
-        for (int i = 0; i < word1.length; i++) {
-            words1.append(word1[i]);
-        }
-
-        StringBuilder words2 = new StringBuilder();
-        for (int i = 0; i < word2.length; i++) {
-            words2.append(word2[i]);
-        }
-
-        return words1.toString().equals(words2.toString());
+        return String.join("", word1).equals(String.join("", word2));
     }
 }
