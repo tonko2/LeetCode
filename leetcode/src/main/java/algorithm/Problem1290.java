e1fa5b46cc8d550e92dea6a005c87aff1b28201b
tonko2 <10cherry07@gmail.com>
2021-05-05 13:02:44 +0900

    ListNode class is in Problem1290 class

--- leetcode/src/main/java/algorithm/Problem1290.java
+++ leetcode/src/main/java/algorithm/Problem1290.java
@@ -12,21 +12,22 @@ public class Problem1290 {
         }
         return ans;
     }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
