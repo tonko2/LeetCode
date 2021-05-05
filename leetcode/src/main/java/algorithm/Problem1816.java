5a2a58255dcee838477aa5db1dde7eeae6cc6afc
tonko2 <10cherry07@gmail.com>
2021-05-05 13:29:50 +0900

    Add problem 1816

--- leetcode/src/main/java/algorithm/Problem1816.java
+++ leetcode/src/main/java/algorithm/Problem1816.java
@@ -1,4 +1,15 @@
 package algorithm;
 
 public class Problem1816 {
+    public String truncateSentence(String s, int k) {
+        StringBuilder ans = new StringBuilder();
+        String[] words = s.split(" ");
+        for(int i=0; i<k; i++) {
+            if (i != 0){
+                ans.append(" ");
+            }
+            ans.append(words[i]);
+        }
+        return ans.toString();
+    }
 }
