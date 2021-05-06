25367dcb94955fd0ac5cd104702d8a97e87b9804
tonko2 <10cherry07@gmail.com>
2021-05-06 23:10:34 +0900

    Add problem 1704

--- leetcode/src/main/java/algorithm/Problem1704.java
+++ leetcode/src/main/java/algorithm/Problem1704.java
@@ -1,4 +1,30 @@
 package algorithm;
 
 public class Problem1704 {
+    private static final char[] VOWELS = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+    public boolean halvesAreAlike(String s) {
+        int cnt = 0;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (isVowels(s.charAt(i))) {
+                cnt++;
+            }
+        }
+
+        for(int i=s.length() / 2; i < s.length(); i++) {
+            if (isVowels(s.charAt(i))) {
+                cnt--;
+            }
+        }
+        return cnt == 0;
+    }
+
+    private boolean isVowels(char c) {
+        for(int i=0; i<VOWELS.length; i++) {
+            if (c == VOWELS[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
