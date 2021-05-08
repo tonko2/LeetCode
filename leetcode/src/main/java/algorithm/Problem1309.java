220ef2511d6330798e866a9721aa4d8df58e806d
tonko2 <10cherry07@gmail.com>
2021-05-08 19:06:49 +0900

    Add problem 1309

--- leetcode/src/main/java/algorithm/Problem1309.java
+++ leetcode/src/main/java/algorithm/Problem1309.java
@@ -1,4 +1,17 @@
 package algorithm;
 
 public class Problem1309 {
+    public String freqAlphabets(String s) {
+        StringBuilder ans = new StringBuilder();
+        for(int i=s.length()-1; i>=0; i--) {
+            if (s.charAt(i) == '#') {
+                int plus = Integer.valueOf(s.substring(i-2, i));
+                ans.append((char)('a' + plus));
+                i-=2;
+            } else {
+                ans.append((char)('a' + (s.charAt(i) - '1')));
+            }
+        }
+        return ans.reverse().toString();
+    }
 }
