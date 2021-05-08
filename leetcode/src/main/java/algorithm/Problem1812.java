a054e144ee9d596f18fa439290b3bdd6f056d778
tonko2 <10cherry07@gmail.com>
2021-05-08 13:16:24 +0900

    Add problem 1812

--- leetcode/src/main/java/algorithm/Problem1812.java
+++ leetcode/src/main/java/algorithm/Problem1812.java
@@ -1,4 +1,13 @@
 package algorithm;
 
 public class Problem1812 {
+    public boolean squareIsWhite(String coordinates) {
+        int x = coordinates.charAt(0) - 'a';
+        int y = coordinates.charAt(1) - '1';
+        if (x % 2 == 0) {
+            return y % 2 != 0;
+        } else {
+            return y % 2 == 0;
+        }
+    }
 }
