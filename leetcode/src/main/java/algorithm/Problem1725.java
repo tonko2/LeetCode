afea8b2989efa36e2f7bf2fddff61172825a7d61
tonko2 <10cherry07@gmail.com>
2021-05-08 13:42:22 +0900

    Add problem 1725

--- leetcode/src/main/java/algorithm/Problem1725.java
+++ leetcode/src/main/java/algorithm/Problem1725.java
@@ -1,4 +1,20 @@
 package algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem1725 {
+    public int countGoodRectangles(int[][] rectangles) {
+        int ans = 0;
+        int maxL = 0;
+        for(int i=0; i<rectangles.length; i++) {
+            maxL = Math.max(maxL, Math.min(rectangles[i][0], rectangles[i][1]));
+        }
+        for(int i=0; i<rectangles.length; i++) {
+            if (Math.min(rectangles[i][0], rectangles[i][1]) == maxL) {
+                ans++;
+            }
+        }
+        return ans;
+    }
 }
