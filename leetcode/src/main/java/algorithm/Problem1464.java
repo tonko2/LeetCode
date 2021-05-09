6226a1bec5e914418d867971d23b251b83fc2186
tonko2 <10cherry07@gmail.com>
2021-05-09 22:07:32 +0900

    Add problem 1464

--- leetcode/src/main/java/algorithm/Problem1464.java
+++ leetcode/src/main/java/algorithm/Problem1464.java
@@ -1,4 +1,19 @@
 package algorithm;
 
+import java.util.Arrays;
+
 public class Problem1464 {
+    public int maxProduct(int[] nums) {
+        int maxFirst = -1;
+        int maxSecond = -1;
+        for(int i=0; i<nums.length; i++) {
+            if (maxFirst < nums[i]) {
+                maxSecond = maxFirst;
+                maxFirst = nums[i];
+            } else if (maxSecond < nums[i]) {
+                maxSecond = nums[i];
+            }
+        }
+        return (maxFirst - 1) * (maxSecond - 1);
+    }
 }
