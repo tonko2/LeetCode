242aaa61d66fd92a7881d719d503e02d35b7a3de
tonko2 <10cherry07@gmail.com>
2021-05-11 23:08:15 +0900

    Add problem 617

---
+++ leetcode/src/main/java/algorithm/Problem617.java
@@ -0,0 +1,38 @@
+package algorithm;
+
+public class Problem617 {
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return null;
+        } else if (root1 == null) {
+            return root2;
+        } else if (root2 == null) {
+            return root1;
+        }
+        TreeNode newNode = new TreeNode(root1.val + root2.val, null, null);
+        newNode.left = mergeTrees(root1.left, root2.left);
+        newNode.right = mergeTrees(root1.right, root2.right);
+        return newNode;
+    }
+
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
