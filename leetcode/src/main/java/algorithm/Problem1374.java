44340699a3b7f11fd7a28d61afe82b8c2d128953
tonko2 <10cherry07@gmail.com>
2021-05-11 21:33:15 +0900

    Add problem 1374

--- leetcode/src/main/java/algorithm/Problem1374.java
+++ leetcode/src/main/java/algorithm/Problem1374.java
@@ -1,15 +1,7 @@
 package algorithm;
 
-import org.springframework.util.StringUtils;
-
-import java.util.Collections;
-
 public class Problem1374 {
     public String generateTheString(int n) {
-        if (n % 2 == 1) {
-            return String.join("", Collections.nCopies(n, "a"));
-        } else {
-            return "b" + String.join("", Collections.nCopies(n-1, "a"));
-        }
+        return "a".repeat(n-1)
     }
 }
