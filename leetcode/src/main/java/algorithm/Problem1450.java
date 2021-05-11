e267e55b74074222b4920a4b0ffda4b43fcbd1ee
tonko2 <10cherry07@gmail.com>
2021-05-11 21:36:56 +0900

    Add problem 1450

---
+++ leetcode/src/main/java/algorithm/Problem1450.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+public class Problem1450 {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int n = startTime.length;
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
