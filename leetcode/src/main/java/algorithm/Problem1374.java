ceb5946e68e305c0fedb24c4e35439caa6a1cf1f
tonko2 <10cherry07@gmail.com>
2021-05-11 21:34:31 +0900

    Modified problem 1374

--- leetcode/src/main/java/algorithm/Problem1374.java
+++ leetcode/src/main/java/algorithm/Problem1374.java
@@ -1,7 +1,13 @@
 package algorithm;
 
+import java.util.Collections;
+
 public class Problem1374 {
     public String generateTheString(int n) {
-        return "a".repeat(n-1)
+        if (n % 2 == 1) {
+            return String.join("", Collections.nCopies(n, "a"));
+        } else {
+            return "b" + String.join("", Collections.nCopies(n - 1, "a"));
+        }
     }
 }
