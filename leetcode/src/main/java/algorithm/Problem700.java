da11a2885d24edf0d7d806c21e17f1428d6727e3
Yuya Kusaka <10cherry07@gmail.com>
2021-05-16 16:33:47 +0900

    Add problem 700

---
+++ leetcode/src/main/java/algorithm/Problem700.java
@@ -0,0 +1,36 @@
+package algorithm;
+
+public class Problem700 {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (val == root.val) {
+            return root;
+        } else if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
