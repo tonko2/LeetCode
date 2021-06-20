731004bdbde4e22d73ed4547c352117b2717e7ed
Yuya Kusaka <10cherry07@gmail.com>
2021-06-21 08:51:44 +0900

    Add problem 1217

---
+++ leetcode/src/main/java/algorithm/Problem1217.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+public class Problem1217 {
+    public int minCostToMoveChips(int[] position) {
+        int even = 0;
+        int odd = 0;
+        for (int pos : position) {
+            if (pos % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        return Math.min(even, odd);
+    }
+}
