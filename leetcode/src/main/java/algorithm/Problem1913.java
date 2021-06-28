63c72be345d87f003a2899af3e1ce7a7fcc516c7
Yuya Kusaka <10cherry07@gmail.com>
2021-06-29 08:51:53 +0900

    Add problem 1913

---
+++ leetcode/src/main/java/algorithm/Problem1913.java
@@ -0,0 +1,10 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class Problem1913 {
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1];
+    }
+}
