cc3e25e6f75185f356675cfe257158e8ac6b6742
Yuya Kusaka <10cherry07@gmail.com>
2021-06-29 09:08:00 +0900

    Add problem 1656

---
+++ leetcode/src/main/java/algorithm/Problem1656.java
@@ -0,0 +1,28 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem1656 {
+    class OrderedStream {
+        int ptr;
+        String[] res;
+
+        public OrderedStream(int n) {
+            ptr = 0;
+            res = new String[n];
+        }
+
+        public List<String> insert(int id, String value) {
+            List<String> list = new ArrayList<>();
+
+            res[id - 1] = value;
+            while (ptr < res.length && res[ptr] != null) {
+                list.add(res[ptr]);
+                ptr++;
+            }
+
+            return list;
+        }
+    }
+}
