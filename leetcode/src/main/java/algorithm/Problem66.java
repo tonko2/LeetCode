7608b6ecd0053dc0c0f727b2fecbb471f2cd4c1d
Yuya Kusaka <10cherry07@gmail.com>
2021-08-31 17:10:13 +0900

    Add problem 66

---
+++ leetcode/src/main/java/algorithm/Problem66.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem66 {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length - 1; i>=0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                break;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        if (digits[0] == 0) {
+            int res[] = new int[digits.length + 1];
+            res[0] = 1;
+            return res;
+        }
+        return digits;
+    }
+}
